f80da95485e997113020965634549fc2944ef2fb
Abby Abraha <Tyeser@gmail.com>
2019-01-16 13:34:10 -0800

    Added alertAndDate function to JavaScript file. Edited html to include alertAndDate function

--- JavaScrip.js
+++ JavaScrip.js
@@ -10,3 +10,17 @@ function myFunction() {
                     var dateTime = date+' '+time;
                     return dateTime;
 }
+function alertAndLogDate(){
+    
+1. get date and time and store in variable because we are using the value for two different method call
+
+    var date = myFunction();
+    
+2. send alert with that date clear Timeout
+    
+    alert(date);
+
+3. log the date to console   
+    console.log(Date());
+
+}
